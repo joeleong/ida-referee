08de57d0dfe5a579ffb615ed5a82b92e67497b85
user <user@users-MacBook-Air.local>
2017-09-18 15:26:06 -0400

    WIP add comments back

--- referee.py
+++ referee.py
@@ -10,14 +10,6 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger("referee")
 
 
-es = []
-xs = []
-
-
-def clear_output_window():
-    idaapi.process_ui_action('msglist:Clear')
-
-
 def is_assn(t):
     return (
         t == idaapi.cot_asg or
@@ -33,34 +25,6 @@ def is_assn(t):
         t == idaapi.cot_asgsmod or
         t == idaapi.cot_asgumod)
 
-
-def flags_to_str(num):
-    match = []
-    if num & idaapi.dr_R == idaapi.dr_R:
-        match.append('dr_R')
-        num ^= idaapi.dr_R
-    if num & idaapi.dr_O == idaapi.dr_O:
-        match.append('dr_O')
-        num ^= idaapi.dr_O
-    if num & idaapi.dr_W == idaapi.dr_W:
-        match.append('dr_W')
-        num ^= idaapi.dr_W
-    if num & idaapi.dr_I == idaapi.dr_I:
-        match.append('dr_I')
-        num ^= idaapi.dr_I
-    if num & idaapi.dr_T == idaapi.dr_T:
-        match.append('dr_T')
-        num ^= idaapi.dr_T
-    if num & idaapi.XREF_USER == idaapi.XREF_USER:
-        match.append('XREF_USER')
-        num ^= idaapi.XREF_USER
-    if num & idaapi.XREF_DATA == idaapi.XREF_DATA:
-        match.append('XREF_DATA')
-        num ^= idaapi.XREF_DATA
-    res = ' | '.join(match)
-    if num:
-        res += ' unknown: 0x{:X}'.format(num)
-    return res
 
 def add_struct_xrefs(cfunc):
     class xref_adder_t(idaapi.ctree_visitor_t):
@@ -83,37 +47,18 @@ def add_struct_xrefs(cfunc):
                         break
             return ea
 
-
         def visit_expr(self, e):
             dr = idaapi.dr_R | idaapi.XREF_USER
-
             ea = self.find_addr(e)
-            # if typ.is_struct():
-            #     strname = typ.dstr()
-            #     if strname.startswith("struct "):
-            #         strname = strname[len("struct "):]
-            #     stid = idaapi.get_struc_id(strname)
-            #     s = idaapi.get_struc(stid)
-                # log.warn("STRUCT {:>6}: {:>3} 0x{:010X} {:>10}".format(e.opname, len(es), ea, e.type.dstr()))
 
-                # if s:
-                #
-                #
-                #     parent = self.cfunc.body.find_parent_of(e)
-                #     grandparent = self.cfunc.body.find_parent_of(parent)
-                #     if ((parent and (is_assn(parent.op) and parent.cexpr.x == e)) or
-                #        (parent.op in (idaapi.cot_memref, idaapi.cot_memptr) and
-                #             grandparent and is_assn(grandparent.op) and
-                #             grandparent.cexpr.x == parent.cexpr)):
-                #         dr = idaapi.dr_W | idaapi.XREF_USER
-                #         es.append(e)
-                #         xs.append((ea, stid, dr))
-                #         idaapi.add_dref(ea, stid, dr)
-                #         log.debug(("xref from 0x{:X} "
-                #                    "to struct {} "
-                #                    "({})").format(
-                #                    ea, strname, flags_to_str(dr)))
+            # We wish to know what context a struct usage occurs in
+            # so we can determine what kind of xref to create. Unfortunately,
+            # a post-order traversal makes this difficult.
 
+            # For assignments, we visit the left, instead
+            # Note that immediate lvalues will be visited twice,
+            # and will be eronneously marked with a read dref.
+            # However, it is safer to overapproximate than underapproximate
             if is_assn(e.op):
                 e = e.x
                 dr = idaapi.dr_W | idaapi.XREF_USER
@@ -128,20 +73,23 @@ def add_struct_xrefs(cfunc):
                   e.op == idaapi.cot_memptr):
                 moff = e.m
 
+                # The only way I could figure out how
+                # to get the structure/member associated with its use
                 typ = e.x.type
+
                 if e.op == idaapi.cot_memptr:
                     typ.remove_ptr_or_array()
+
                 strname = typ.dstr()
                 if strname.startswith("struct "):
                     strname = strname[len("struct "):]
+
                 stid = idaapi.get_struc_id(strname)
                 s = idaapi.get_struc(stid)
-
                 mem = idaapi.get_member(s, moff)
-                if s:
+
+                if s is not None:
                     if (ea, stid) not in self.xrefs or dr < self.xrefs[(ea, stid)]:
-                        es.append(e)
-                        xs.append((ea, stid, dr))
                         self.xrefs[(ea, stid)] = dr
                         idaapi.add_dref(ea, stid, dr)
                         log.debug((" 0x{:X} \t"
@@ -149,11 +97,8 @@ def add_struct_xrefs(cfunc):
                                    "{}").format(
                                    ea, strname, flags_to_str(dr)))
 
-                    # idaapi.add_dref(ea, stid, dr)
-                    if mem:
+                    if mem is not None:
                         if (ea, mem.id) not in self.xrefs or dr < self.xrefs[(ea, mem.id)]:
-                            es.append(e)
-                            xs.append((ea, mem.id, dr))
                             self.xrefs[(ea, mem.id)] = dr
                             idaapi.add_dref(ea, mem.id, dr)
                             log.debug((" 0x{:X} \t"
@@ -164,11 +109,12 @@ def add_struct_xrefs(cfunc):
                                        flags_to_str(dr)))
 
                 else:
-                    log.error(("xref failure from 0x{:X} "
-                               "to struct {} (id: 0x{:X}) ({})").format(
+                    log.error(("failure from 0x{:X} "
+                               "on struct {} (id: 0x{:X}) {}").format(
                                ea, strname, stid, flags_to_str(dr)))
 
             return 0
+
     adder = xref_adder_t(cfunc)
     adder.apply_to_exprs(cfunc.body, None)
 
@@ -214,6 +160,7 @@ class Referee(idaapi.plugin_t):
         return idaapi.PLUGIN_KEEP
 
     def run(self, arg):
+        # never called
         pass
 
     def term(self):
@@ -224,3 +171,36 @@ class Referee(idaapi.plugin_t):
 
 def PLUGIN_ENTRY():
     return Referee()
+
+
+def flags_to_str(num):
+    match = []
+    if num & idaapi.dr_R == idaapi.dr_R:
+        match.append('dr_R')
+        num ^= idaapi.dr_R
+    if num & idaapi.dr_O == idaapi.dr_O:
+        match.append('dr_O')
+        num ^= idaapi.dr_O
+    if num & idaapi.dr_W == idaapi.dr_W:
+        match.append('dr_W')
+        num ^= idaapi.dr_W
+    if num & idaapi.dr_I == idaapi.dr_I:
+        match.append('dr_I')
+        num ^= idaapi.dr_I
+    if num & idaapi.dr_T == idaapi.dr_T:
+        match.append('dr_T')
+        num ^= idaapi.dr_T
+    if num & idaapi.XREF_USER == idaapi.XREF_USER:
+        match.append('XREF_USER')
+        num ^= idaapi.XREF_USER
+    if num & idaapi.XREF_DATA == idaapi.XREF_DATA:
+        match.append('XREF_DATA')
+        num ^= idaapi.XREF_DATA
+    res = ' | '.join(match)
+    if num:
+        res += ' unknown: 0x{:X}'.format(num)
+    return res
+
+
+def clear_output_window():
+    idaapi.process_ui_action('msglist:Clear')
